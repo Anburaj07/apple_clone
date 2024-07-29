3606a6dcdfe45539f61374e487969480d6bf516e
Anburaj S <anburaj2203@gmail.com>
2024-07-29 16:18:54 +0530

    added responsiveness to the navbar

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 const HomePage = () => {
   return <div>
+    <h1 className="font-bold border-4 border-blue-500 mt-14 h-96">HomePage</h1>
+    <h1 className="font-bold border-4 border-blue-500 mt-10 h-96">HomePage</h1>
+    <h1 className="font-bold border-4 border-blue-500 mt-10 h-96">HomePage</h1>
     <h1 className="font-bold border-4 border-blue-500 mt-10 h-96">HomePage</h1>
   </div>;
 };
