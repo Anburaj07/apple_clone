c2f1d11a74e276be98c2d72d9fd2f745c0e9c927
Anburaj S <anburaj2203@gmail.com>
2024-07-29 14:31:09 +0530

    created navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
+const navItems = [
+  "Store",
+  "Mac",
+  "iPad",
+  "iPhone",
+  "Watch",
+  "Vision",
+  "AirPods",
+  "TV & Home",
+  "Entertainment",
+  "Accessories",
+  "Support",
+  "🔍",
+  "💼",
+];
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-[#434344] text-[#c4c4c5] text-center p-2 py-3 fixed top-0 left-0 w-full z-50">
+      <div className="w-[70%] m-auto flex justify-between text-xs font-semibold">
+        <p>logo</p>
+        {navItems.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomePage = () => {
   return <div>
-    <h1 className="font-bold">HomePage</h1>
+    <h1 className="font-bold border-4 border-blue-500 mt-10 h-96">HomePage</h1>
   </div>;
 };
 
